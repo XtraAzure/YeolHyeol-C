7e88376c33b8f6143e410333f7802c4caa14cbc3
XtraAzure <35952292+XtraAzure@users.noreply.github.com>
2018-07-15 04:59:49 +0900

    Add files via upload

---
+++ Chapter 22/22-7 Question2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct employee
+{
+	char name[20]; // 이름
+	char pid[20]; //주민번호
+	int salary; //급여
+};
+
+int main()
+{
+	struct employee arr[3];
+	int i;
+
+	for (i = 0; 3 > i; i++)
+	{
+		printf("이름: ");
+		scanf("%s", arr[i].name);
+		printf("주민번호: ");
+		scanf("%s", arr[i].pid);
+		printf("급여: ");
+		scanf("%d", &arr[i].salary);
+	}
+
+	for (i = 0; 3 > i; i++)
+	{
+		printf("이름: %s \n", arr[i].name);
+		printf("주민번호: %s \n", arr[i].pid);
+		printf("급여: %d \n", &arr[i].salary);
+	}
+	return 0;
+}
